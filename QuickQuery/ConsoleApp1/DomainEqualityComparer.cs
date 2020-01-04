b6aa7608e5f08eeba7f7450499ddc97afd3b5703
luozhi <1052231926@qq.com>
2020-01-04 21:33:50 +0800

    基础构建

---
+++ QuickQuery/ConsoleApp1/DomainEqualityComparer.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace OQ.ORM
+{
+    /// <summary>
+    /// 域名比较器,支持泛解析(*.jioukeji.com)
+    /// </summary>
+    public class DomainEqualityComparer : IEqualityComparer<string>
+    {
+        public bool Equals(string x, string y)
+        {
+            bool flag = string.IsNullOrEmpty(x) || string.IsNullOrEmpty(y);
+            bool result;
+            if (flag)
+            {
+                result = false;
+            }
+            else
+            {
+                bool flag2 = x == y;
+                if (flag2)
+                {
+                    result = true;
+                }
+                else
+                {
+                    bool flag3 = x.StartsWith("*");
+                    if (flag3)
+                    {
+                        Regex regex = new Regex(".+" + x.Substring(1).Replace(".", "\\."));
+                        bool flag4 = regex.IsMatch(y);
+                        result = flag4;
+                    }
+                    else
+                    {
+                        result = false;
+                    }
+                }
+            }
+            return result;
+        }
+
+        public int GetHashCode(string obj)
+        {
+            return obj.GetHashCode();
+        }
+    }
+}
